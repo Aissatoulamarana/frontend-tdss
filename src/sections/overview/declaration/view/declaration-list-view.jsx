0b4dfe311d7d40c97857b85d7f978072735f4bbc
aissatouLamarana <aissatoul.diallo@fasosmart.com>
2025-07-02 13:54:13 +0000

    barre de recherche avec des options dans la liste des déclarations

--- src/sections/overview/declaration/view/declaration-list-view.jsx
+++ src/sections/overview/declaration/view/declaration-list-view.jsx
@@ -56,6 +56,7 @@ import { DeclarationTableToolbar } from '../declaration-table-toolbar';
 
 import { useMockedUser } from 'src/auth/hooks';
 import dayjs from 'dayjs';
+import { number } from 'prop-types';
 
 // ----------------------------------------------------------------------
 
@@ -91,7 +92,7 @@ export function DeclarationListView() {
   const [tableData, setTableData] = useState([]);
   const [loading, setLoading] = useState(true); // État pour indiquer le chargement
   const [error, setError] = useState(null); // État pour gérer les erreurs
-  const [selectedFilter, setSelectedFilter] = useState('title'); // options de recherche
+  const [selectedFilter, setSelectedFilter] = useState('number'); // options de recherche
   const [count, setCount] = useState();
 
   const [pagination, setPagination] = useState({
@@ -103,7 +104,7 @@ export function DeclarationListView() {
   const [summary, setSummary] = useState({ totalCount: 0, countByStatus: {} });
 
   const filters = useSetState({
-    name: '', // mot-clé pour filtrer par numéro ou type de déclaration
+    number: '', // mot-clé pour filtrer par numéro ou type de déclaration
     fonction: [],
     title: '',
     company: '',
@@ -125,7 +126,8 @@ export function DeclarationListView() {
   const dataInPage = rowInPage(dataFiltered, table.page, table.rowsPerPage);
 
   const canReset =
-    !!filters.state.type ||
+    !!filters.state.number ||
+    // !!filters.state.type ||
     !!filters.state.title ||
     !!filters.state.company ||
     !!filters.state.passport_number ||
@@ -532,13 +534,15 @@ export function DeclarationListView() {
             ? { title: filters.state.title }
             : filters.state.passport_number
               ? { passport_number: filters.state.passport_number }
+              :filters.state.number
+                ? { number: filters.state.number }
               : {}
         ),
 
           ...(filters.state.status !== 'all' ? { status: filters.state.status } : {}),
           ...(filters.state.starts_at && filters.state.ends_at && !dateError
             ? {
-                starts_at: dayjs(filters.state.dstarts_at).format('YYYY-MM-DD '),
+                starts_at: dayjs(filters.state.starts_at).format('YYYY-MM-DD '),
                 ends_at: dayjs(filters.state.ends_at).format('YYYY-MM-DD '),
               }
             : {}),
@@ -565,7 +569,16 @@ export function DeclarationListView() {
 
     fetchDeclarations();
 
-  }, [table.page, table.rowsPerPage, filters.state.company, filters.state.title, filters.state.passport_number, filters.state.status, filters.state.starts_at, filters.state.ends_at]);
+  }, [
+    table.page, 
+    table.rowsPerPage, 
+    filters.state.number,
+    filters.state.company, 
+    filters.state.title, 
+    filters.state.passport_number, 
+    filters.state.status, 
+    filters.state.starts_at, 
+    filters.state.ends_at]);
 
 
   if (loading) {
