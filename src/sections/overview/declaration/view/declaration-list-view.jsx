f43d2b2552aace00e949ec96b0309e9c8438a3cf
banofsmart <mbano.barry@fasosmart.com>
2025-07-07 17:05:33 +0000

    Ajout de la navigation vers les détails d'un employé au clic sur une ligne, sauf sur la Checkbox et la cellule Nom & Prénom

--- src/sections/overview/declaration/view/declaration-list-view.jsx
+++ src/sections/overview/declaration/view/declaration-list-view.jsx
@@ -557,7 +557,7 @@ export function DeclarationListView() {
           params.ends_at = endOfDay.format('YYYY-MM-DD');
         }
 
-        console.log('Fetching declarations with params:', params);
+        // console.log('Fetching declarations with params:', params);
         const response = await axios.get(API.listDeclarations(), { params });
         
         setTableData(response.data.results);
