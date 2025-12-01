3a86ce9288541788e21868a71b3133b5ea9f2c64
aissatouLamarana <aissatoul.diallo@fasosmart.com>
2025-12-01 22:35:19 +0000

    feat: add biometric data management component and update permit API endpoint

--- src/utils/api.js
+++ src/utils/api.js
@@ -167,6 +167,8 @@ const API = {
   submitPermit: (slug) => `${BASE_URL}/declarations/employees/${slug}/submit/`,
   unsubmitPermit: (slug) => `${BASE_URL}/declarations/employees/${slug}/unsubmit/`,
   validatePermit: (slug) => `${BASE_URL}/declarations/employees/${slug}/validate/`,
+  updatePermit: (declarationSlug, employeeSlug) =>
+    `${BASE_URL}/declarations/${declarationSlug}/employees/${employeeSlug}/`,
 
   // Listes des api pour le plan de panafricanisation
 
