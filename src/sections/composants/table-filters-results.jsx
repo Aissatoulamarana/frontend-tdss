924b2223d5103a4fdfdc8efb32d0134b3170770f
aissatouLamarana <aissatoul.diallo@fasosmart.com>
2025-12-01 09:12:13 +0000

    feat: implement filtering options for permits and enhance table toolbar functionality

--- src/sections/composants/table-filters-results.jsx
+++ src/sections/composants/table-filters-results.jsx
@@ -6,40 +6,37 @@ import { chipProps, FiltersBlock, FiltersResult } from 'src/components/filters-r
 // ----------------------------------------------------------------------
 
 export function TableFiltersResult({ filters, onResetPage, totalResults, sx }) {
-    const handleRemoveKeyword = useCallback(() => {
-        onResetPage();
-        filters.setState({ name: '' });
-    }, [filters, onResetPage]);
+  const handleRemoveKeyword = useCallback(() => {
+    onResetPage();
+    filters.setState({ name: '' });
+  }, [filters, onResetPage]);
 
+  const handleRemoveRole = useCallback(
+    (inputValue) => {
+      const newValue = filters?.state?.profil?.filter((item) => item !== inputValue);
 
-    const handleRemoveRole = useCallback(
-        (inputValue) => {
-            const newValue = filters?.state?.profil.filter((item) => item !== inputValue);
+      onResetPage();
+      filters.setState({ profil: newValue });
+    },
+    [filters, onResetPage]
+  );
 
-            onResetPage();
-            filters.setState({ profil: newValue });
-        },
-        [filters, onResetPage]
-    );
+  const handleReset = useCallback(() => {
+    onResetPage();
+    filters.onResetState();
+  }, [filters, onResetPage]);
 
-    const handleReset = useCallback(() => {
-        onResetPage();
-        filters.onResetState();
-    }, [filters, onResetPage]);
+  return (
+    <FiltersResult totalResults={totalResults} onReset={handleReset} sx={sx}>
+      <FiltersBlock label="Profil:" isShow={!!filters?.state?.profil?.length}>
+        {filters?.state?.profil?.map((item) => (
+          <Chip {...chipProps} key={item} label={item} onDelete={() => handleRemoveRole(item)} />
+        ))}
+      </FiltersBlock>
 
-    return (
-        <FiltersResult totalResults={totalResults} onReset={handleReset} sx={sx}>
-
-
-            <FiltersBlock label="Profil:" isShow={!!filters?.state?.profil.length}>
-                {filters?.state?.profil.map((item) => (
-                    <Chip {...chipProps} key={item} label={item} onDelete={() => handleRemoveRole(item)} />
-                ))}
-            </FiltersBlock>
-
-            <FiltersBlock label="Keyword:" isShow={!!filters?.state.name}>
-                <Chip {...chipProps} label={filters?.state?.name} onDelete={handleRemoveKeyword} />
-            </FiltersBlock>
-        </FiltersResult>
-    );
+      <FiltersBlock label="Keyword:" isShow={!!filters?.state?.name}>
+        <Chip {...chipProps} label={filters?.state?.name} onDelete={handleRemoveKeyword} />
+      </FiltersBlock>
+    </FiltersResult>
+  );
 }
