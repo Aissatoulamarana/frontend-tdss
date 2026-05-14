4ac8ec6377e2d6dc9a2b3af6c51a323d8a18e5d5
aissatouLamarana <aissatoul.diallo@fasosmart.com>
2026-05-14 14:41:20 +0000

    repositionnement du numero de la carte

--- src/sections/overview/permit-employee/permit-print.jsx
+++ src/sections/overview/permit-employee/permit-print.jsx
@@ -346,8 +346,8 @@ export function WorkPermitCard({ permit, onClose, open, onPrint }) {
 
 
           <!-- NUMÉRO DE CARTE en haut à droite -->
-          <div style="position: absolute; top: 15mm; left: 50mm; font-size: 3mm; font-weight: 700; color: #000;">
-             ${permit?.card_number}
+          <div style="position: absolute; top: 14mm; left: 51.5mm; font-size: 3mm; font-weight: 700; color: #000;">
+            N°${permit?.card_number}
           </div>
         </div>
       </div>
