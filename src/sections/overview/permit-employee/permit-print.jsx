8d7da41b59284f2ef133535a393dcd823db8cfc7
aissatouLamarana <aissatoul.diallo@fasosmart.com>
2026-05-14 13:11:17 +0000

    ajout de N° devant le numero de la carte en haut

--- src/sections/overview/permit-employee/permit-print.jsx
+++ src/sections/overview/permit-employee/permit-print.jsx
@@ -347,7 +347,7 @@ export function WorkPermitCard({ permit, onClose, open, onPrint }) {
 
           <!-- NUMÉRO DE CARTE en haut à droite -->
           <div style="position: absolute; top: 15mm; left: 50mm; font-size: 3mm; font-weight: 700; color: #000;">
-             ${permit?.card_number}
+             N°${permit?.card_number}
           </div>
         </div>
       </div>
